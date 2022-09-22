be92114662c98a9e783f33336ec1a2cfdfbe9322
fxnt <1330672764@qq.com>
2022-09-22 16:34:03 +0800

    add client

--- pkg/client/verify/verify.go
+++ pkg/client/verify/verify.go
@@ -89,3 +89,22 @@ func VerifyCode(
 	}
 	return nil
 }
+
+func VerifyGoogleRecaptchaV3(
+	ctx context.Context,
+	recaptchaToken string,
+) error {
+	err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) error {
+		_, err := cli.VerifyGoogleRecaptchaV3(ctx, &npool.VerifyGoogleRecaptchaV3Request{
+			RecaptchaToken: recaptchaToken,
+		})
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
