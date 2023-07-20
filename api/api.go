9bddff303de0031af25dffd37f1a6337438c6d66
jakys <jakysing@163.com>
2023-07-20 11:06:43 +0000

    Add oauth api

--- api/api.go
+++ api/api.go
@@ -5,6 +5,7 @@ import (
 
 	v1 "github.com/NpoolPlatform/message/npool/third/mw/v1"
 
+	"github.com/NpoolPlatform/third-middleware/api/v1/oauth"
 	"github.com/NpoolPlatform/third-middleware/api/v1/send"
 	"github.com/NpoolPlatform/third-middleware/api/v1/verify"
 
@@ -20,6 +21,7 @@ func Register(server grpc.ServiceRegistrar) {
 	v1.RegisterMiddlewareServer(server, &Server{})
 	verify.Register(server)
 	send.Register(server)
+	oauth.Register(server)
 }
 
 func RegisterGateway(mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) error {
