86cc36af8eb67d4aebaf68ae0923280700f007d0
jakys <jakysing@163.com>
2023-07-31 01:28:17 +0000

    Fix type

--- pkg/github/oauth.go
+++ pkg/github/oauth.go
@@ -7,11 +7,12 @@ import (
 	"os"
 
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+	npool "github.com/NpoolPlatform/message/npool/third/mw/v1/oauth"
 	constant "github.com/NpoolPlatform/third-middleware/pkg/const"
 	"github.com/go-resty/resty/v2"
 )
 
-func GetAccessToken(clientID, clientSecret, code string) (map[string]interface{}, error) {
+func GetAccessToken(clientID, clientSecret, code string) (*npool.AccessTokenInfo, error) {
 	var err error
 	reqURL := fmt.Sprintf(
 		"%s?client_id=%s&client_secret=%s&code=%s",
@@ -44,17 +45,20 @@ func GetAccessToken(clientID, clientSecret, code string) (map[string]interface{}
 		return nil, err
 	}
 
-	var token = make(map[string]interface{})
-	for key, values := range queryParams {
-		if len(values) > 0 {
-			token[key] = values[0]
-		}
+	errStr := queryParams.Get("error_description")
+	if errStr != "" {
+		return nil, fmt.Errorf("%s", errStr)
 	}
 
-	return token, nil
+	return &npool.AccessTokenInfo{
+		AccessToken:        queryParams.Get("access_token"),
+		RefreshAccessToken: queryParams.Get("refresh_token"),
+		Scope:              queryParams.Get("scope"),
+		TokenType:          queryParams.Get("token_type"),
+	}, nil
 }
 
-func GetUserInfo(accessToken string) (map[string]interface{}, error) {
+func GetUserInfo(accessToken string) (*npool.ThirdUserInfo, error) {
 	var err error
 
 	socksProxy := os.Getenv("ENV_RECAPTCHA_REQUEST_PROXY")
@@ -77,7 +81,7 @@ func GetUserInfo(accessToken string) (map[string]interface{}, error) {
 
 	if !resp.IsSuccess() {
 		logger.Sugar().Error("resp error: ", resp.StatusCode())
-		return nil, err
+		return nil, fmt.Errorf("resp error: %v", resp.StatusCode())
 	}
 
 	var userInfo = make(map[string]interface{})
@@ -86,5 +90,37 @@ func GetUserInfo(accessToken string) (map[string]interface{}, error) {
 		return nil, err
 	}
 
-	return userInfo, nil
+	if userInfo["message"] != nil {
+		messageByte, err := json.Marshal(userInfo["message"])
+		if err != nil {
+			return nil, fmt.Errorf("get message error")
+		}
+		errStr := string(messageByte)
+		return nil, fmt.Errorf("%s", errStr)
+	}
+
+	idByte, err := json.Marshal(userInfo["id"])
+	if err != nil {
+		return nil, fmt.Errorf("get id error")
+	}
+	nameByte, err := json.MarshalIndent(userInfo["name"], "", "  ")
+	if err != nil {
+		return nil, fmt.Errorf("get name error")
+	}
+	loginByte, err := json.MarshalIndent(userInfo["login"], "", "  ")
+	if err != nil {
+		return nil, fmt.Errorf("get login error")
+	}
+	avatarURLByte, err := json.MarshalIndent(userInfo["avatar_url"], "", "  ")
+	if err != nil {
+		return nil, fmt.Errorf("get avatarURL error")
+	}
+	info := &npool.ThirdUserInfo{
+		ID:        string(idByte),
+		Name:      string(nameByte),
+		Login:     string(loginByte),
+		AvatarURL: string(avatarURLByte),
+	}
+
+	return info, nil
 }
--- pkg/mw/oauth/oauth.go
+++ pkg/mw/oauth/oauth.go
@@ -2,7 +2,6 @@ package oauth
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 
 	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
@@ -17,26 +16,11 @@ func (h *Handler) GetAccessToken(ctx context.Context) (*npool.AccessTokenInfo, e
 
 	switch h.ClientName {
 	case basetypes.SignMethod_Github:
-		jsonMap, err := github.GetAccessToken(h.ClientID, h.ClientSecret, h.Code)
+		info, err := github.GetAccessToken(h.ClientID, h.ClientSecret, h.Code)
 		if err != nil {
 			return nil, err
 		}
-		if jsonMap["error_description"] != nil {
-			errByte, err := json.Marshal(jsonMap["error_description"])
-			if err != nil {
-				return nil, fmt.Errorf("get error_description error")
-			}
-			errStr := string(errByte)
-			return nil, fmt.Errorf("%s", errStr)
-		}
-		accessTokenByte, err := json.Marshal(jsonMap["access_token"])
-		if err != nil {
-			return nil, fmt.Errorf("get accesstoken error")
-		}
-		accessTokenStr := string(accessTokenByte)
-		return &npool.AccessTokenInfo{
-			AccessToken: accessTokenStr,
-		}, nil
+		return info, nil
 	default:
 		return nil, fmt.Errorf("unsupport oauth")
 	}
@@ -49,41 +33,10 @@ func (h *Handler) GetThirdUserInfo(ctx context.Context) (*npool.ThirdUserInfo, e
 
 	switch h.ClientName {
 	case basetypes.SignMethod_Github:
-		jsonMap, err := github.GetUserInfo(h.AccessToken)
+		info, err := github.GetUserInfo(h.AccessToken)
 		if err != nil {
 			return nil, err
 		}
-		if jsonMap["message"] != nil {
-			messageByte, err := json.Marshal(jsonMap["message"])
-			if err != nil {
-				return nil, fmt.Errorf("get message error")
-			}
-			errStr := string(messageByte)
-			return nil, fmt.Errorf("%s", errStr)
-		}
-		idByte, err := json.Marshal(jsonMap["id"])
-		if err != nil {
-			return nil, fmt.Errorf("get id error")
-		}
-		nameByte, err := json.Marshal(jsonMap["name"])
-		if err != nil {
-			return nil, fmt.Errorf("get name error")
-		}
-		loginByte, err := json.Marshal(jsonMap["login"])
-		if err != nil {
-			return nil, fmt.Errorf("get login error")
-		}
-		avatarURLByte, err := json.Marshal(jsonMap["avatar_url"])
-		if err != nil {
-			return nil, fmt.Errorf("get avatarURL error")
-		}
-		info := &npool.ThirdUserInfo{
-			ID:        string(idByte),
-			Name:      string(nameByte),
-			Login:     string(loginByte),
-			AvatarURL: string(avatarURLByte),
-		}
-
 		return info, nil
 	default:
 		return nil, fmt.Errorf("unsupport oauth")
